4edea4eaeab57eab10248c6f0615589128a33cf3
EAinsley <vhtmscyo@gmail.com>
2020-09-29 00:38:37 +0800

    optimize code structure.

--- analysis.py
+++ analysis.py
@@ -2,58 +2,55 @@ from typing import List, Tuple
 
 
 def read_csv_for_data(filename: str):
-    """
-    Task 0
+    """read and pre-process the data
 
-    @filename: the file name of the file you need to read in;
+    Args:
+        filename: the file name of the file need to be read in.
 
-    @return: your custom data structure.
+    Returns:
+        A list of dictionaries whose keys and values correspond to the field
+        name and field content respectively.
     """
     with open(filename) as f:
         data = f.read()
-    # you may check input by uncommenting next line
-    # print(data)
-    # Dispose blank line, and separate lines
     list_data = [s.split(",") for s in data.split()]
-    list_dict = []
     # Map each line to dictionary
     list_dict = list([{key: (value.strip()) for key, value in zip(
-        list_data[0], i)}for i in list_data[1:]])
+        list_data[0], i)} for i in list_data[1:]])
     return list_dict
-    raise NotImplementedError
 
 
 def task1(filename: str):
-    """
-    Task 1
+    """Task 1
 
-    @filename: the file name of the data file.
+    Args:
+        filename: the file name of the data file.
 
-    @return: a list of flight number strings.
+    Returns: 
+        A list of flight number strings.
     """
-    # Read in the data from the CSV file `filename` with tools you build in task 0.
     data = read_csv_for_data(filename)
     # Pick out the valid data
-    flight_list = list(map(lambda datum: (datum["AIRLINE"], datum["FLIGHT_NUMBER"], datum["DISTANCE"]), filter(
-        lambda data: int(data["DISTANCE"]) > 1500, data)))
+    flight_list = list(
+        map(lambda datum: (datum["AIRLINE"]+datum["FLIGHT_NUMBER"], datum["DISTANCE"]), filter(
+            lambda data: int(data["DISTANCE"]) > 1500, data)))
     # Handling the format
-    flight_list.sort(key=lambda it: (int(it[2]), it[0] + it[1]))
-    final_list = list(map(lambda datum: datum[0] + datum[1],
-                          flight_list))
+    flight_list.sort(key=lambda it: (int(it[1]), it[0]))
+    final_list = [x[0] for x in flight_list]
     return final_list
-    raise NotImplementedError
 
 
 def task2(filename: str, airline: str, key: str, value) -> int:
-    """
-    Task 2
+    """Task 2
 
-    @filename: the file name of the data file.
-    @airline: the airline as a string of IATA code.
-    @key: the key that we want to filter against.
-    @value: the value that we want to filter against.
+    Args:
+        filename: the file name of the data file.
+        airline: the airline as a string of IATA code.
+        key: the key that we want to filter against.
+        value: the value that we want to filter against.
 
-    @return: the number of flights that satisfy as an integer.
+    Returns: 
+        The number of flights that satisfy as an integer.
     """
     # Try to convert string to integer
     def convert(key_):
@@ -63,50 +60,44 @@ def task2(filename: str, airline: str, key: str, value) -> int:
         except ValueError:
             return key_
 
-    # Read in the data from the CSV file `filename` with tools you build in task 0.
     data = read_csv_for_data(filename)
     # Pick out selected data
     selected_data = filter(lambda airline_: airline_[
                            "AIRLINE"] == airline, data)
     # Calculate by giving key and value
     value = convert(value)
-    count = 0
-    for raw_datum in selected_data:
-        datum = convert(raw_datum[key])
-        if datum < value:
-            count += 1
+    count = sum(convert(datum[key]) < value for datum in selected_data)
     return count
-    raise NotImplementedError
 
 
 def task3(filename: str) -> List[Tuple[str, float]]:
-    """
-    Task 3
+    """Task 3
+    Args:
+        filename: the file name of the data file.
 
-    @filename: the file name of the data file.
-
-    @return: a list of tuples with format of (airline, rate).
+    Returns: 
+        A list of tuples with format of (airline, rate).
     """
-   # Read in the data.
     data = read_csv_for_data(filename)
     # Claculate on-time rate
     airline_dict = {}
     for datum in data:
-        if datum["AIRLINE"] not in airline_dict:
-            airline_dict[datum["AIRLINE"]] = [0, 1]
-        else:
+        try:
             airline_dict[datum["AIRLINE"]][1] += 1
-        if datum["ARRIVAL_DELAY"][0] == "-":
-            airline_dict[datum["AIRLINE"]][0] += 1
+        except:
+            airline_dict[datum["AIRLINE"]] = [0, 1]
+        finally:
+            if datum["ARRIVAL_DELAY"][0] == "-":
+                airline_dict[datum["AIRLINE"]][0] += 1
     airline_list = list(map(lambda dicts: (
         dicts[0], dicts[1][0]/dicts[1][1]), airline_dict.items()))
     # Sort the on-time rate
     airline_list.sort(key=lambda it: it[0])
     airline_list.sort(key=lambda it: it[1], reverse=True)
     return list(airline_list)
-    raise NotImplementedError
 
 
 if __name__ == "__main__":
     print(task1(r"data/sample.csv"))
+    print(task2(r"data/sample.csv", "AA", "TAIL_NUMBER", "NKUABa"))
     pass
