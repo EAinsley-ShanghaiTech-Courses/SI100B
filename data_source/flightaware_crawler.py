a01afc283357c09948c1a7fd05fb9ef3a2feb05a
EAinsley <vhtmscyo@gmail.com>
2020-12-08 20:50:01 +0800

    完成爬虫部分

--- data_source/flightaware_crawler.py
+++ data_source/flightaware_crawler.py
@@ -21,35 +21,48 @@ class FlightAwareCrawler:
         self.latitude_nw, self.longitude_nw = rng
         self.latitude_se, self.longitude_se = (x * 2 - y
                                                for x, y in zip(loc, rng))
+        print(self.latitude_se, self.longitude_se)
 
     def get_data_once(self):
         # !无法跨越180经线求和，如果跨越180度经线，需要分成两部分求解
-        # !在输入数据完全合法的情况下，只需考虑两种情况：右下角经度大于180，
-        # !右下角纬度小于-90
+        # !在输入数据完全合法的情况下，只需考虑两种特殊情况：右下角经度大于180，
+        # !右下角纬度小于-90（这个无法处理）
         tokenregx = '\"VICINITY_TOKEN\":\"([A-Za-z0-9]*)\"'
         kDataURL = "https://flightaware.com/ajax/vicinity_aircraft.rvt"
         kHtmlURL = "https://flightaware.com/live/"
-        token = re.search(tokenregx, kHtmlURL.text)
+        html = rq.get(kHtmlURL)
+        token = re.search(tokenregx, html.text)
         print(token.group(1))
 
-        testparam = {
-            "minLon": 10,
-            "minLat": -8.59954833984375,
-            "maxLon": -10,
-            "maxLat": 27.0703125,
+        params = {
+            "minLon": self.longitude_nw,
+            "minLat": max(-90, self.latitude_se),
+            "maxLon": min(180, self.longitude_se),
+            "maxLat": self.latitude_nw,
             "token": token.group(1)
         }
+        query_data_raw = rq.get(kDataURL, params=params).json()
+        print(query_data_raw.keys())
+        query_data = query_data_raw["features"]
 
-        testquery = rq.get(kDataURL, params=testparam)
+        # 经线跨国180度
+        if self.longitude_se > 180:
+            params["minLat"] = -180
+            params["maxLat"] = self.longitude_se - 360
+            query_data_raw = rq.get(kDataURL, params=params).json()
+            query_data.extend(query_data_raw["features"])
 
-        print(testquery)
-        t = testquery.json()
-        print(len(t['features']))
-        t['features'][0]
+        # TODO：筛选数据
+
+        print(query_data)
 
     def spin(self, interval=1):
+        start_time = time.time()
         self.get_data_once()
-        time.sleep(interval)
+        time.sleep(interval - time.time() + start_time)
 
 
-a = FlightAwareCrawler((1.1, 1.1), (0.0, 0.0))
+a = FlightAwareCrawler((31.17940, 121.59043), (51.0, 100.0))
+# 31.17940N, 121.59043E
+
+a.get_data_once()
