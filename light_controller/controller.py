e65e8798077bad119b7e31e18e66abec534f5b14
EAinsley <vhtmscyo@gmail.com>
2020-12-15 15:51:08 +0800

    完成light controller

--- light_controller/controller.py
+++ light_controller/controller.py
@@ -5,17 +5,18 @@
 #                      Author: Diao Zihao <hi@ericdiao.com>                    #
 #                          Last motified: 07/07/2020                           #
 # ============================================================================ #
+from gpiozero import PWMLED
 
 
 class BaseController:
     def __init__(self):
-        raise NotImplementedError
+        self.digit[4] = [
+            PWMLED(i, initial_value=0, frequency=120) for i in range(14, 18)
+        ]
 
-    def work_once(self):
-        raise NotImplementedError
-
-    def on(self):
-        raise NotImplementedError
-
-    def off(self):
-        raise NotImplementedError
+    def work_once(self, num):
+        print("NUM:", num)
+        num = max(num, 16)
+        for i in range(4):
+            self.digit[i].value = num % 2
+            num //= 2
--- state.py
+++ state.py
@@ -1,18 +1,34 @@
-# ============================================================================ #
-#                               Python Project                                 #
-#        SI 100B: Introduction to Information Science and Technology           #
-#                        Fall 2020, ShanghaiTech University                    #
-#                      Author: Diao Zihao <hi@ericdiao.com>                    #
-#                          Last motified: 07/07/2020                           #
-# ============================================================================ #
-# from typing import Tuple
-# from data_source.fr24_crawler import Fr24Crawler
-# from light_controller.controller import *
+#!/usr/bin/env python3
+from data_source.flightaware_crawler import FlightAwareCrawler
+from light_controller.controller import BaseController
+import time
+import json
 
 
 class State:
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, filename='/tmp/data.json'):
+        self.__file = filename
+        self.__controller = BaseController()
 
-    def spin(self):
-        raise NotImplementedError
+    def spin(self, interval=1, max_loop=None):
+        start_time = time.time()
+        loop_count = 0
+        while max_loop is None or loop_count < max_loop:
+            print("Loading data...")
+            with open(self.__file) as f:
+                data = json.load(f)
+            num = len(data)
+            self.__controller.work_once(num)
+            time.sleep(max(interval - time.time + start_time, 0))
+            loop_count += 1
+
+
+if __name__ == '__main__':
+    kLatitude = 31.17940
+    kLongitude = 121.59043
+    for i in range(5):
+        crl = FlightAwareCrawler((kLatitude, kLongitude),
+                                 (kLatitude + 0.25, kLongitude - 0.25))
+        crl.spin(max_loop=1)
+        state = State()
+        state.spin(max_loop=1)
