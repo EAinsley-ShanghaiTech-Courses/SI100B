3c006e742892034c01febc9cbe7acb7ff56542f8
Iven <yanyh1@shanghaitech.edu.cn>
2020-12-24 21:25:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #       SI 100B: Introduction to Information Science and Technology            #
 #                       Fall 2020, ShanghaiTech University                     #
 #                     Author: Diao Zihao <hi@ericdiao.com>                     #
-#                         Last motified: 07/07/2020                            #
+#                         Last modified: 07/07/2020                            #
 # ============================================================================ #
 
 # NOTE to student:
